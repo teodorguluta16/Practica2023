9869806f5e7cbe3a30c281c86654915da6a14043
teodorguluta16 <128083123+teodorguluta16@users.noreply.github.com>
2023-06-27 09:43:40 +0300

    Update fotbal.py

--- pages/fotbal.py
+++ pages/fotbal.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
 
 import streamlit as st
-st.title("Fotbal")
+
+st.set_page_config(page_title="Football", page_icon=":soccer:", layout="wide")
+st.title("Football :soccer:")
+st.markdown("##")
+
 
 coduri=[]
+optiuni=['-']
 
 def atribuie():
     file_path = r'C:\Users\Teo G\Desktop\proiect2\coduri.txt'
@@ -15,13 +21,10 @@ def atribuie():
     for line in lines:
         values = line.split()
         coduri.append((values[0], int(values[1])))
-
+        optiuni.append(values[0])
 atribuie()
-optiuni=['-']
-optiuni.append('real-madrid')
-optiuni.append('barcelona')
-optiuni.append('ajax')
-optiuni.append('_La-Liga')
+
+
 selected_option = st.selectbox("Alege o echipa/campionat", optiuni)
 
 for ops in optiuni:
@@ -40,11 +43,8 @@ for ops in optiuni:
                     if nume_cautat==ops:
                         cod=id_cautat
                         break
-                print(cod)
 
                 url = "https://www.sofascore.ro/team/football/{}/{}/".format(ops, cod)
-                print(url)
-                print(url)
                 response = requests.get(url)
                 soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -116,34 +116,197 @@ for ops in optiuni:
                 span_text = result
                 st.write(span_text)
             else:
+                cod = -1
+                for it in coduri:
+                    nume_cautat = it[0]
+                    id_cautat = it[1]
+                    if nume_cautat == ops:
+                        cod = id_cautat
+                        break
                 new_string = ops[1:]
                 ops=new_string
                 sir = "Informatii esentiale despre " + ops
                 size = 30
                 st.markdown(f'<span style="color: blue; font-weight: bold; font-size: {size}px">{sir}</span>',
                             unsafe_allow_html=True)
-                st.write("Loading...")
 
-                standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
-                data = requests.get(standings_url)
-                soup = BeautifulSoup(data.text, 'html.parser')
-                standings_table = soup.select('table.stats_table')[0]
-                links = standings_table.find_all('a')
-                links = [l.get("href") for l in links]
-                links = [l for l in links if '/squads/' in l]
-                team_urls = [f"https://fbref.com{l}" for l in links]
-                data = requests.get(team_urls[0])
-                import pandas as pd
-                matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
-                soup = BeautifulSoup(data.text)
-                links = soup.find_all('a')
-                links = [l.get("href") for l in links]
-                links = [l for l in links if l and 'all_comps/shooting/' in l]
-                data = requests.get(f"https://fbref.com{links[0]}")
-                shooting = pd.read_html(data.text, match="Shooting")[0]
-                shooting.columns = shooting.columns.droplevel()
-                team_data = matches.merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")
-                st.write(team_data.head())
-                years = list(range(2022, 2020, -1))
-                all_matches = []
-                standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+
+                url = 'https://fbref.com/en/comps/{}/{}-Stats'.format(cod,ops)
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                table = soup.find('table', {'id': 'results2022-2023{}1_overall'.format(cod)})
+
+                rankings = []
+                teams = []
+                matches_played = []
+                wins = []
+                draws = []
+                losses = []
+                goals_for = []
+                goals_against = []
+                goal_diffs = []
+                points = []
+                points_match = []
+                xG = []
+                xGA = []
+                xGD = []
+                xGD90 = []
+                top_goals = []
+                notes = []
+                team_logos = []
+                spectators= []
+                rows = table.find_all('tr')
+                for row in rows[1:]:
+                    columns = row.find_all('td')
+                    teams.append(columns[0].text.strip())
+                    matches_played.append(columns[1].text.strip())
+                    wins.append(columns[2].text.strip())
+                    draws.append(columns[3].text.strip())
+                    losses.append(columns[4].text.strip())
+                    goals_for.append(columns[5].text.strip())
+                    goals_against.append(columns[6].text.strip())
+                    goal_diffs.append(columns[7].text.strip())
+                    points.append(columns[8].text.strip())
+                    points_match.append(columns[9].text.strip())
+                    xG.append(columns[10].text.strip())
+                    xGA.append(columns[11].text.strip())
+                    xGD.append(columns[12].text.strip())
+                    xGD90.append(columns[13].text.strip())
+                    spectators.append(columns[14].text.strip())
+                    top_goals.append(columns[15].text.strip())
+                    notes.append(columns[16].text.strip())
+
+                    logo_url = columns[0].find('img')['src']
+                    team_logos.append(logo_url)
+
+                row_indices = list(range(1, len(teams) + 1))
+                data = {
+                    'Rank': row_indices,
+                    'Team': teams,
+                    'Matches Played': matches_played,
+                    'Wins': wins,
+                    'Draws': draws,
+                    'Losses': losses,
+                    'Goals For': goals_for,
+                    'Goals Against': goals_against,
+                    'Goal Difference': goal_diffs,
+                    'Points': points,
+                }
+
+                data2={
+                    'Rank': row_indices,
+                    'Team': teams,
+                    'Matches Played': matches_played,
+                    'Wins': wins,
+                    'Draws': draws,
+                    'Losses': losses,
+                    'Goals For': goals_for,
+                    'Goals Against': goals_against,
+                    'Goal Difference': goal_diffs,
+                    'Points': points,
+                    'Point/match':points_match,
+                    'xG':xG,
+                    'xGA':xGA,
+                    'xGD':xGD,
+                    'xGD90':xGD90,
+                    'Spectators':spectators,
+                    'Top_goals':top_goals,
+                    'Notes':notes
+                }
+                df = pd.DataFrame(data)
+                styled_table = df.style.applymap(lambda x: 'color: green; background-color: yellow')
+
+
+                df2 = pd.DataFrame(data2)
+
+                df2.to_excel("Championship Ranking.xlsx")
+
+                goluri = [int(num) for num in goals_for]  # Convert string numbers to integers
+                sum = sum(goluri)
+                average = sum / len(goluri)
+
+                top_maractori = []
+                topNrgoluri = []
+
+                for scorer in top_goals:
+                    numbers = [int(s) for s in scorer.split() if s.isdigit()]
+                    print(numbers)
+                    topNrgoluri.append(numbers)
+                    nume = scorer.split("-")[0].strip()
+                    top_maractori.append(nume)
+
+                max_number = max(topNrgoluri)
+                marcator = ""
+                for index, string in enumerate(topNrgoluri):
+                    if topNrgoluri[index] == max_number:
+                        marcator = top_maractori[index]
+                        break
+
+                most_assists_element = soup.find('strong', text='Most Assists')
+                if most_assists_element:
+                    most_assists_text = most_assists_element.find_next_sibling('a').text + "  " + most_assists_element.find_next_sibling('span').text
+
+                most_clean_sheets_element = soup.find('strong',text='Most Clean Sheets')
+                if most_clean_sheets_element:
+                    most_sheets_text = most_clean_sheets_element.find_next_sibling('a').text + "  " + most_clean_sheets_element.find_next_sibling('span').text
+
+                champion_element = soup.find('strong', text='Champion')
+                if champion_element:
+                    champion_info = champion_element.find_next('a').text
+
+                logo_element = soup.find('img', class_='teamlogo')
+                if logo_element:
+                    logo_url = logo_element['src']
+                st.image(logo_url)
+
+                champion_logo_element = soup.find('p', string="Champion")
+                if champion_logo_element:
+                    logo_element = champion_logo_element.find_previous('img', class_='teamlogo')
+                    if logo_element:
+                        logo_url = logo_element['src']
+                left_column, middle_column, right_column = st.columns(3)
+                with left_column:
+                    st.markdown("<h3 style='color: yellow;'>Total goals:</h3>", unsafe_allow_html=True)
+                    st.subheader(str(sum))
+                    st.markdown("<h3 style='color: yellow;'>Nr goals/per stage:</h3>", unsafe_allow_html=True)
+                    st.subheader(str(average))
+                with middle_column:
+                    st.markdown("<h3 style='color: yellow;'>The best player scorer:</h3>", unsafe_allow_html=True)
+                    st.subheader(marcator + " " + str(max_number[0]))
+                    st.markdown("<h3 style='color: yellow;'>Most Assists:</h3>", unsafe_allow_html=True)
+                    st.subheader(most_assists_text)
+                with right_column:
+                    st.markdown("<h3 style='color: yellow;'>Most clean sheets:</h3>", unsafe_allow_html=True)
+                    st.subheader(most_sheets_text)
+                    st.markdown("<h3 style='color: yellow;'>Champion:</h3>", unsafe_allow_html=True)
+                    st.subheader(champion_info)
+                    st.image(team_logos[0])
+
+                st.markdown("""---""")
+
+                st.table(styled_table)
+
+                import matplotlib.pyplot as plt
+
+                short_name_teams=[]
+                for team in teams:
+                    result = team.split()[0]
+                    short_name_teams.append(result)
+                int_spec = []
+                for string in spectators:
+                    string_without_comma = ''.join(string.split(','))
+                    integer_value = int(string_without_comma)
+                    int_spec.append(integer_value)
+                st.title("Number of spectators per match")
+
+                fig, ax = plt.subplots(figsize=(17, 7))
+                ax.bar(short_name_teams, int_spec)
+                ax.set_xlabel('Echipa')
+                ax.set_ylabel('Nr. Spectators')
+                st.pyplot(fig)
+
+
+
+
+
+
